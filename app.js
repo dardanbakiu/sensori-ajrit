6c2262bc8951d97f6ba26f339fba16355dbe4ac5
DardanB <>
2022-11-29 23:06:15 +0100

    front + back

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
-const router = require("./router/index");
+const router = require("./router");
 require("dotenv").config();
 
 app.use(cors());
---
+++ controller/TestController.js
@@ -0,0 +1,7 @@
+const testFunction = (req, res) => {
+  res.json({'name': 'grupi i sensorit te ajrit'})
+}
+
+module.exports = {
+  testFunction
+};
