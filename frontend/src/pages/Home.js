c69e0390ab3e63d2e41da222c9a398e5d1474dfb
DardanB <>
2022-12-26 12:33:48 +0100

    through time boxes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,9 +4,8 @@ import Text from '../components/text/Text'
 import Box from '../components/box/Box'
   
 const Home = () => {
-  
-
   return (
+
     <div>
       <Header />
       <Text 
@@ -19,21 +18,99 @@ const Home = () => {
       <div>
         <div style={{display:'flex', justifyContent: 'space-evenly', padding: '20px 0'}}>
           <Box background='#57bf9c'> 
-            <p>hellow rold</p>
-          </Box>
+          <Text color="white" weight='800' content='Tani' position='center'/>
+            <div style={{
+                color:'white',
+                backgroundColor: 'inherit',
+                display: 'flex',
+                alignItems: 'center'
+            }}> 
+              
+              <div style={{margin: '0 5px', backgroundColor: 'inherit'}}>
+                <Text color="white" weight='600' content='56' size='45px'/>
+              </div>
+
+              <div style={{margin: '0 10px', backgroundColor: 'inherit'}}>
+                <Text color="white" weight='800' content='microgram'/>
+              </div>
+            </div>
+            
+            <div style={{margin: '0px', backgroundColor: 'inherit'}}>
+              <Text color="white" weight='800' content='Good' position='center' size='20px'/>
+            </div>
+          </Box>  
 
           <Box background='#57bf9c'> 
-            <p>hellow rold</p>
-          </Box>
+          <Text color="white" weight='800' content='Sot' position='center'/>
+            <div style={{
+                color:'white',
+                backgroundColor: 'inherit',
+                display: 'flex',
+                alignItems: 'center'
+            }}> 
+              
+              <div style={{margin: '0 5px', backgroundColor: 'inherit'}}>
+                <Text color="white" weight='600' content='56' size='45px'/>
+              </div>
+
+              <div style={{margin: '0 10px', backgroundColor: 'inherit'}}>
+                <Text color="white" weight='800' content='microgram'/>
+              </div>
+            </div>
+            
+            <div style={{margin: '0px', backgroundColor: 'inherit'}}>
+              <Text color="white" weight='800' content='Good' position='center' size='20px'/>
+            </div>
+          </Box> 
         </div>
 
         <div style={{display:'flex', justifyContent: 'space-evenly', padding: '20px 0'}}>
+        <Box background='#57bf9c'> 
+          <Text color="white" weight='800' content='Gjat Javes' position='center'/>
+            <div style={{
+                color:'white',
+                backgroundColor: 'inherit',
+                display: 'flex',
+                alignItems: 'center'
+            }}> 
+              
+              <div style={{margin: '0 5px', backgroundColor: 'inherit'}}>
+                <Text color="white" weight='600' content='56' size='45px'/>
+              </div>
+
+              <div style={{margin: '0 10px', backgroundColor: 'inherit'}}>
+                <Text color="white" weight='800' content='microgram'/>
+              </div>
+            </div>
+            
+            <div style={{margin: '0px', backgroundColor: 'inherit'}}>
+              <Text color="white" weight='800' content='Good' position='center' size='20px'/>
+            </div>
+          </Box> 
+          
+          
           <Box background='#57bf9c'> 
-            <p>hellow rold</p>
-          </Box>
-          <Box background='#57bf9c'> 
-            <p>hellow rold</p>
-          </Box>
+          <Text color="white" weight='800' content='Gjat Muajit' position='center'/>
+            <div style={{
+                color:'white',
+                backgroundColor: 'inherit',
+                display: 'flex',
+                alignItems: 'center'
+            }}> 
+              
+              <div style={{margin: '0 5px', backgroundColor: 'inherit'}}>
+                <Text color="white" weight='600' content='56' size='45px'/>
+              </div>
+
+              <div style={{margin: '0 10px', backgroundColor: 'inherit'}}>
+                <Text color="white" weight='800' content='microgram'/>
+              </div>
+            </div>
+            
+            <div style={{margin: '0px', backgroundColor: 'inherit'}}>
+              <Text color="white" weight='800' content='Good' position='center' size='20px'/>
+            </div>
+          </Box> 
         </div>
       </div>
     
